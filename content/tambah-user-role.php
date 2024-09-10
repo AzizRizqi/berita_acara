d11a66aa078e1440fa0fa841717bd91b8d965ce6
AzizRizqi <azizrizqi2005@gmail.com>
2024-09-10 09:54:12 +0700

    middleware

--- content/tambah-user-role.php
+++ content/tambah-user-role.php
@@ -3,17 +3,9 @@ if (isset($_POST['simpan'])) {
     $level_id = $_POST['level_id'];
     $user_id = $_GET['id_user'];
     $insert = mysqli_query($koneksi, "INSERT INTO user_levels (level_id, user_id)VALUES ('$level_id','$user_id') ");
-    header("location:?pg=user-role&id_user=".urlencode($user_id)."&tambah=berhasil");
+    header("location:?pg=user-role&id_user=" . urlencode($user_id) . "&tambah=berhasil");
 }
-if (isset($_GET['delete'])) {
-    $id = $_GET['delete'];
 
-    // print_r($id);
-    // die;
-
-    $delete = mysqli_query($koneksi, "DELETE FROM users WHERE id='$id'");
-    header("location:?pg=user&hapus=berhasil");
-}
 // $id = $_GET['edit']??''; <= cara di sebelah itu buat solve "edit" cara lain dari yang di bawah
 
 // isset= tidak kosong / isinya ada
@@ -24,19 +16,33 @@ if (isset($_GET['edit'])) {
     // print_r($id);
     // die;
 
-    $edit = mysqli_query($koneksi, "SELECT * FROM users WHERE id='$id'");
+    $edit = mysqli_query($koneksi, "SELECT * FROM user_levels WHERE id='$id'");
     $rowEdit = mysqli_fetch_assoc($edit);
+}
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
 
-    if (isset($_POST['edit'])) {
-        $fullname = $_POST['fullname'];
-        $email = $_POST['email'];
-        $password = sha1($_POST['password']);
-        $update = mysqli_query($koneksi, "UPDATE users SET fullname='$fullname',email ='$email',password='$password' WHERE id='$id'");
-        header("location:?pg=user&ubah=berhasil");
-    }
+    
+    $edit = mysqli_query($koneksi, "SELECT * FROM user_levels WHERE id='$id'");
+    $rowEdit = mysqli_fetch_assoc($edit);
+    // print_r($id);
+    // die;
+
+    $user_id = $rowEdit['user_id'];
+
+    $delete = mysqli_query($koneksi, "DELETE FROM user_levels WHERE id='$id'");
+    header("location:?pg=user-role&id_user=" . $user_id . "&hapus=berhasil");
+}
+if (isset($_POST['edit'])) {
+    $user_id = $rowEdit['user_id'];
+    $level_id = $_POST['level_id'];
+    $id = $_GET['edit'];
+    $update = mysqli_query($koneksi, "UPDATE user_levels SET level_id='$level_id' WHERE id='$id'");
+    header("location:?pg=user-role&id_user=" . $user_id . "&ubah=berhasil");
 }
 
-    $querylevels = mysqli_query($koneksi, "SELECT * FROM levels ORDER BY id DESC");
+
+$querylevels = mysqli_query($koneksi, "SELECT * FROM levels ORDER BY id DESC");
 ?>
 
 <form action="" method="post">
@@ -46,13 +52,13 @@ if (isset($_GET['edit'])) {
         </div>
         <select name="level_id" id="" class="form-control">
             <option value="">Pilih Level</option>
-            <?php while ($rowLevel= mysqli_fetch_assoc($querylevels)): ?>
-            <option value="<?php echo $rowLevel['id'] ?>"><?php echo $rowLevel['level_name'] ?></option>
-            <?php endwhile;?>
+            <?php while ($rowLevel = mysqli_fetch_assoc($querylevels)): ?>
+                <option <?php echo isset($rowEdit) ? ($rowEdit['level_id'] == $rowLevel['id']) ? 'selected' : '' : ''  ?> value="<?php echo $rowLevel['id'] ?>"><?php echo $rowLevel['level_name'] ?></option>
+            <?php endwhile; ?>
         </select>
     </div>
     <div class="mb-3">
         <button type="submit" class="btn btn-primary" name="<?php echo isset($_GET['edit']) ? 'edit' : 'simpan' ?>"><?php echo isset($_GET['edit']) ? 'ubah' : 'simpan' ?></button>
-        <a href="?pg=user" class="btn btn-secondary">Kembali</a>
+        <a href="?pg=tambah-user-role" class="btn btn-secondary">Kembali</a>
     </div>
 </form>
--- content/user-role.php
+++ content/user-role.php
@@ -1,6 +1,6 @@
 <?php
 $user_id = $_GET['id_user'] ?? '';
-$queryUser = mysqli_query($koneksi, "SELECT * FROM user_levels 
+$queryUser = mysqli_query($koneksi, "SELECT users.fullname,levels.level_name,user_levels. * FROM user_levels 
 LEFT JOIN users ON users.id = user_levels.user_id 
 LEFT JOIN levels ON levels.id = user_levels.level_id 
 WHERE user_id = '$user_id' ORDER BY user_levels.id DESC");
@@ -26,9 +26,9 @@ WHERE user_id = '$user_id' ORDER BY user_levels.id DESC");
                 <td><?php echo $rowUser['level_name'] ?></td>
                 <td>
                     
-                    <a href="?pg=tambah-user&edit=<?php echo $rowUser['id'] ?>" class="btn btn-success">Edit </a>
+                    <a href="?pg=tambah-user-role&edit=<?php echo $rowUser['id'] ?>" class="btn btn-success">Edit </a>
 
-                    <a class="btn btn-danger" onclick="return confirm('Apakah anda yakin cuy menghapus data nih??')" href="?pg=tambah-user&delete=<?php echo $rowUser['id'] ?>">Delete</a>
+                    <a class="btn btn-danger" onclick="return confirm('Apakah anda yakin cuy menghapus data nih??')" href="?pg=tambah-user-role&delete=<?php echo $rowUser['id'] ?>">Delete</a>
                 </td>
             </tr>
         <?php endwhile ?>
